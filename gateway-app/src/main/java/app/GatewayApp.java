9a874d5ab34cc7f9527e7e205178467953f9c811
Zeljko Markovic <velox1s@yahoo.com>
2022-03-26 01:59:24 +0100

    Configured initial in-memory authentication

--- gateway-app/src/main/java/app/GatewayApp.java
+++ gateway-app/src/main/java/app/GatewayApp.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableZuulProxy
+@ComponentScan(basePackages = {"controller", "filters", "security"})
 public class GatewayApp {
     public static void main(String[] args)  {
         SpringApplication.run(GatewayApp.class, args);
