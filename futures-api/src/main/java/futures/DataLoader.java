1db020ee4c21f29e64a701bd3dd5879e05263a89
sbudimac <sbudimac@gmail.com>
2022-04-05 13:25:24 +0200

    Uklonjeni nepotrebni importi.

--- futures-api/src/main/java/futures/DataLoader.java
+++ futures-api/src/main/java/futures/DataLoader.java
@@ -4,9 +4,7 @@ import futures.model.FuturesContract;
 import futures.repositories.FuturesContractRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import java.io.BufferedReader;
 import java.io.File;
