62200642f4172a6597ae3ebb8fb5812400454304
mkazandzic17 <mkazandzic17@raf.rs>
2022-04-08 14:11:48 +0200

    Dodat task za fetch podataka sa yahoo finance. Dodata ruta /startDate:endDate. Izbrisana ruta /stock.

--- stocks/src/main/java/com/osiguranje/stocks/model/StockModel.java
+++ stocks/src/main/java/com/osiguranje/stocks/model/StockModel.java
@@ -1,24 +1,28 @@
 package com.osiguranje.stocks.model;
 
 import com.crazzyghost.alphavantage.timeseries.response.StockUnit;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+
 import javax.persistence.*;
+import java.sql.Date;
 
 @Getter
 @Setter
 @ToString
 @Entity
 @Table(name = "stocks")
+@NoArgsConstructor
+@AllArgsConstructor
 public class StockModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, length = 5)
+    @Column(name = "symbol", nullable = false, length = 5)
     private String symbol;
     @Column(nullable = false)
     private Long outstandingShares;
+    @Column(nullable = false)
+    private Date date;
     @Column(nullable = false)
     private Double open;
     @Column(nullable = false)
@@ -30,23 +34,10 @@ public class StockModel {
     @Column(nullable = false)
     private Long volume;
 
-    public StockModel() {
-    }
-
-    public StockModel(StockUnit stockUnit, String symbol, Long outstandingShares) {
+    public StockModel(String symbol, Long outstandingShares, Date date, Double open, Double close, Double low, Double high, Long volume) {
         this.symbol = symbol;
         this.outstandingShares = outstandingShares;
-        this.open = stockUnit.getOpen();
-        this.close = stockUnit.getClose();
-        this.low = stockUnit.getLow();
-        this.high = stockUnit.getHigh();
-        this.volume = stockUnit.getVolume();
-    }
-
-    public StockModel(Long id, String symbol, Long outstandingShares, Double open, Double close, Double low, Double high, Long volume) {
-        this.id = id;
-        this.symbol = symbol;
-        this.outstandingShares = outstandingShares;
+        this.date = date;
         this.open = open;
         this.close = close;
         this.low = low;
---
+++ stocks/src/test/java/com/osiguranje/stocks/StocksApplicationTest.java
@@ -0,0 +1,16 @@
+package com.osiguranje.stocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import springfox.documentation.service.Contact;
+
+import java.util.Collections;
+
+@SpringBootTest
+class StocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
