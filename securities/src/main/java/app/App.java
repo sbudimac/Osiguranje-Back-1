a7fdfc1a31d069054440d6de5e4e0733d67abb0a
ilija <pavlovicilija42@gmail.com>
2022-04-09 20:29:46 +0200

    Fix bean  loading

--- securities/src/main/java/app/App.java
+++ securities/src/main/java/app/App.java
@@ -2,10 +2,14 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"controllers", "configuration", "model", "repositories","services"})
 @EnableJpaRepositories("repositories")
+@EntityScan("model")
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(DataLoader.class, args);
--- securities/src/main/java/repositories/SecurityHistoryRepository.java
+++ securities/src/main/java/repositories/SecurityHistoryRepository.java
@@ -2,6 +2,8 @@ package repositories;
 
 import model.SecurityHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SecurityHistoryRepository extends JpaRepository<SecurityHistory, Long> {
 }
