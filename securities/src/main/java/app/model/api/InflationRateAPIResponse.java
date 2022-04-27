63dbb332e3413e10da2c9b41b73055fcb5f8231f
vranes <nikolinavranes@gmail.com>
2022-04-28 00:30:35 +0200

    Removed unnecessary imports

--- securities/src/main/java/app/model/api/InflationRateAPIResponse.java
+++ securities/src/main/java/app/model/api/InflationRateAPIResponse.java
@@ -2,13 +2,10 @@ package app.model.api;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import javafx.util.Pair;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 @Getter
 @Setter
