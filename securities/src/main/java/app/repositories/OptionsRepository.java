2444f3bc54004701d2039c0012b16dc8b0c32b16
vranes <nikolinavranes@gmail.com>
2022-05-27 17:47:51 +0200

    Renamed Option to StockOption due to MySql syntax

--- securities/src/main/java/app/repositories/OptionsRepository.java
+++ securities/src/main/java/app/repositories/OptionsRepository.java
@@ -1,13 +1,13 @@
 package app.repositories;
 
-import app.model.Option;
+import app.model.StockOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface OptionsRepository extends JpaRepository<Option, Long> {
-    List<Option> findAll();
-    Option save(Option option);
+public interface OptionsRepository extends JpaRepository<StockOption, Long> {
+    List<StockOption> findAll();
+    StockOption save(StockOption option);
 }
