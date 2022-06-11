fa81bca47e835698abb0ae7cb56fc5ac44095a83
vranes <nikolinavranes@gmail.com>
2022-06-11 20:23:56 +0200

    Added id into SecurityDto in buying market

--- buying-market/src/main/java/buyingmarket/model/dto/SecurityDto.java
+++ buying-market/src/main/java/buyingmarket/model/dto/SecurityDto.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 @Getter
 @Setter
 public class SecurityDto {
+    protected int id;
     protected String ticker;
     protected String name;
     protected String lastUpdated;
