1a9bf15b5fd02149964588db55fcbd357da212f9
ilija <pavlovicilija42@gmail.com>
2022-06-28 22:23:20 +0200

    Add otc package and otc models for company, employee and bank account

---
+++ otc/src/main/java/otc/model/Employee.java
@@ -0,0 +1,31 @@
+package otc.model;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    private Long employeeId;
+    @Column
+    private String name;
+    @Column
+    private String surname;
+    @Column
+    private String telNumber;
+    @Column
+    private String email;
+    @Column
+    private String companyPosition;
+    @Column
+    private String description;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "registration_ID", nullable = false)
+    private Company employer;
+}
---
+++ otc/src/main/java/otc/model/dto/Company.java
@@ -0,0 +1,24 @@
+package otc.model.dto;
+
+import lombok.*;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Company {
+    @NotNull
+    private String name;
+    @NotNull
+    private Long registrationID;
+    @NotNull
+    private Long taxID;
+    @NotNull
+    private Long industrialClassificationID;
+    @NotNull
+    private String address;
+    private Set<Employee> employees;
+}
