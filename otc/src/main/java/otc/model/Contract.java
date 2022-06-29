46f605bb610bf13d757b9cc4c70c26a8c4f9ac05
vranes <nikolinavranes@gmail.com>
2022-06-29 19:59:32 +0200

    Progress on otc model

--- otc/src/main/java/otc/model/Contract.java
+++ otc/src/main/java/otc/model/Contract.java
@@ -1,11 +1,9 @@
 package otc.model;
 
 import lombok.*;
-import otc.model.dto.Company;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "contract")
@@ -16,15 +14,33 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 public class Contract {
 
-
-    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
     private Long contractID;
-    private Company employer;
-    @NotNull
-    private String status;
-    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "registration_ID", nullable = false)
+    private Company company;
+    @Column
+    private Status status;
+    @Column
     private String creationDate;
-    private String lastEdit;
-    @NotNull
+    @Column
+    private String lastUpdated;
+    @Column
     private String description;
+    @OneToMany(mappedBy = "transactionID", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Transaction> transactions;
+
+    private enum Status{
+        DRAFT, FINALIZED
+    }
+
+    public Contract(Long contractID, Company company, String creationDate, String lastUpdated, String description) {
+        this.contractID = contractID;
+        this.company = company;
+        this.status = Status.DRAFT;
+        this.creationDate = creationDate;
+        this.lastUpdated = lastUpdated;
+        this.description = description;
+    }
 }
