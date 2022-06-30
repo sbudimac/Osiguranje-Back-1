e2e64f52ba50ae02a58550ef5f7662f0fb2d62e6
mboji <mbojic12@gmail.com>
2022-06-30 19:59:45 +0200

    Service

---
+++ account-transaction/src/main/java/raf/osiguranje/accounttransaction/controllers/TransactionController.java
@@ -0,0 +1,11 @@
+package raf.osiguranje.accounttransaction.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/transaction")
+public class TransactionController {
+}
--- account-transaction/src/main/java/raf/osiguranje/accounttransaction/services/TransactionService.java
+++ account-transaction/src/main/java/raf/osiguranje/accounttransaction/services/TransactionService.java
@@ -3,9 +3,12 @@ package raf.osiguranje.accounttransaction.services;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import raf.osiguranje.accounttransaction.model.Account;
 import raf.osiguranje.accounttransaction.model.Transaction;
 import raf.osiguranje.accounttransaction.model.dto.TransactionDTO;
 import raf.osiguranje.accounttransaction.repositories.TransactionRepository;
+
+import java.util.List;
 
 @NoArgsConstructor
 @Service
@@ -22,9 +25,41 @@ public class TransactionService {
         this.balanceService = balanceService;
     }
 
+    public Transaction getTransactionFromDto(TransactionDTO transactionDTO){
+        return new Transaction(transactionDTO.getAccountId(),transactionDTO.getOrderId(),transactionDTO.getUserId(),transactionDTO.getForexId(),
+                transactionDTO.getPayment(),transactionDTO.getPayout(),transactionDTO.getReserve(), transactionDTO.getUsedReserve());
+    }
+
+
     public boolean createTransaction(TransactionDTO transactionDTO){
+
+        Account tmpAccount = accountService.findAccountById(transactionDTO.getAccountId());
+        if(tmpAccount==null){
+            return false;
+        }
+
+        Transaction transaction = getTransactionFromDto(transactionDTO);
+
+        transactionRepository.save(transaction);
 
         return true;
     }
+
+    public List<Transaction> getTransactionsByAccount(Long input){
+        return transactionRepository.findAllByAccountId(input);
+    }
+
+    public List<Transaction> getTransactionsByOrderId(Long input){
+        return transactionRepository.findAllByOrderId(input);
+    }
+
+    public List<Transaction> getTransactionsByUser(Long input){
+        return transactionRepository.findAllByUserId(input);
+    }
+
+    public List<Transaction> getTransactionsByForex(Long input){
+        return transactionRepository.findAllByForexId(input);
+    }
+
 
 }
