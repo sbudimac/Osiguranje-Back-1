11db751c4088e09eb932b44111f13cf633146a23
ilija <pavlovicilija42@gmail.com>
2022-07-09 17:45:54 +0200

    Fix stock info url in options service

--- securities/src/main/java/app/services/OptionsService.java
+++ securities/src/main/java/app/services/OptionsService.java
@@ -5,6 +5,7 @@ import app.model.StockOption;
 import app.model.api.OptionsAPIResponse;
 import app.model.dto.OptionDTO;
 import app.repositories.OptionsRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -19,63 +20,68 @@ import java.util.*;
 @Service
 public class OptionsService {
     private final OptionsRepository optionsRepository;
+
+    @Value("${api.stockinfo}")
+    public String stockinfoApiUrl;
+
     public OptionsService(OptionsRepository optionsRepository) {
         this.optionsRepository = optionsRepository;
     }
 
-    public StockOption save(StockOption option){
+    public StockOption save(StockOption option) {
         return optionsRepository.save(option);
     }
 
-    public List<OptionDTO> getOptionsDTOData(){
-        List<StockOption> optionList = getOptionsData();
-        List<OptionDTO> dtoList = new ArrayList<>();
-        for (StockOption o: optionList){
+    public List <OptionDTO> getOptionsDTOData() {
+        List <StockOption> optionList = getOptionsData();
+        List <OptionDTO> dtoList = new ArrayList <>();
+        for (StockOption o : optionList) {
             dtoList.add(new OptionDTO(o));
         }
         return dtoList;
     }
 
-    public List<StockOption> getOptionsData() {
+    public List <StockOption> getOptionsData() {
         return optionsRepository.findAll();
     }
 
-    public List<StockOption> updateData() {
+    public List <StockOption> updateData() {
         System.out.println("Updating options");
 
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Date date = new Date();
 
-        List<StockOption> options = getOptionsData();
+        List <StockOption> options = getOptionsData();
 
-        for (StockOption o: options){
+        for (StockOption o : options) {
             o.setLastUpdated(formatter.format(date));
 
             RestTemplate rest = new RestTemplate();
             HttpHeaders headers = new HttpHeaders();
-            HttpEntity<OptionsAPIResponse> entity = new HttpEntity<>(headers);
+            HttpEntity <OptionsAPIResponse> entity = new HttpEntity <>(headers);
 
-            List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+            List <HttpMessageConverter <?>> messageConverters = new ArrayList <>();
             MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
             converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
             messageConverters.add(converter);
             rest.setMessageConverters(messageConverters);
 
-            List<HashMap<String, String>> responseContent;
+            List <HashMap <String, String>> responseContent;
             try {
-                ResponseEntity<OptionsAPIResponse> response = rest.exchange(Config.getProperty("python_service_url") + "options/" + o.getOptionType() + "?symbol=" + o.getTicker(), HttpMethod.GET, entity, OptionsAPIResponse.class);
+                ResponseEntity <OptionsAPIResponse> response = rest.exchange(stockinfoApiUrl + "/options/" + o.getOptionType() + "?symbol=" + o.getTicker(), HttpMethod.GET, entity, OptionsAPIResponse.class);
                 responseContent = Objects.requireNonNull(response.getBody()).getData();
-                if(responseContent.isEmpty())
+                if (responseContent.isEmpty())
                     throw new Exception("Empty response");
             } catch (Exception e) {
                 continue;
             }
 
-            HashMap<String, String> optionMap = responseContent.get(0);
-            try{
+            HashMap <String, String> optionMap = responseContent.get(0);
+            try {
                 o.setOpenInterest(Long.parseLong(optionMap.get("Open Interest").replace("-", "0")));
                 o.setImpliedVolatility(BigDecimal.valueOf(Double.parseDouble(optionMap.get("Implied Volatility").replace("%", "").replace("-", "0").replace(",", ""))));
-            } catch (Exception e){}
+            } catch (Exception e) {
+            }
             optionsRepository.save(o);
         }
         return options;
