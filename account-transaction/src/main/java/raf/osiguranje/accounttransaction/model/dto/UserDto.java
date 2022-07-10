29590b842db06167cf219f9d0a692ceafd1f6ff8
mboji <mbojic12@gmail.com>
2022-07-10 16:25:42 +0200

    Making Sonarcloaud work

--- account-transaction/src/main/java/raf/osiguranje/accounttransaction/model/dto/UserDto.java
+++ account-transaction/src/main/java/raf/osiguranje/accounttransaction/model/dto/UserDto.java
@@ -1,7 +1,6 @@
 package raf.osiguranje.accounttransaction.model.dto;
 
 import lombok.*;
-
 @Setter
 @Getter
 @NoArgsConstructor
@@ -12,9 +11,9 @@ public class UserDto {
     private Long id;
     private String firstName;
     private String lastName;
-    private Boolean active;
     private String email;
     private String position;
     private String phoneNumber;
+    private Boolean active;
 }
 
